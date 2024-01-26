008e17af6eca113ae8220b157c92b8aa73bf0d7e
Taishi-N324 <82321333+Taishi-N324@users.noreply.github.com>
2024-01-27 00:25:37 +0900

    Scripts for evaluation

---
+++ scripts/ylab/bloom/all_run.sh
@@ -0,0 +1,10 @@
+#!/bash/bin
+lang=$1
+model_path=$2
+echo $lang
+echo $model_path
+
+ybatch scripts/ylab/bloom/run_arc.sh $lang $model_path
+ybatch scripts/ylab/bloom/run_hellaswag.sh $lang $model_path
+ybatch scripts/ylab/bloom/run_mmlu.sh $lang $model_path
+ybatch scripts/ylab/bloom/run_truthfulqa.sh $lang $model_path
