f5d4d60cdfccc2cb263354781892cec60531c6ce
Taishi-N324 <taishiahiru5@gmail.com>
2025-06-05 23:41:52 +0900

    Add aggregate script

--- lm_eval/loggers/evaluation_tracker.py
+++ lm_eval/loggers/evaluation_tracker.py
@@ -230,20 +230,7 @@ class EvaluationTracker:
 
                 path = Path(self.output_path if self.output_path else Path.cwd())
                 self.date_id = datetime.now().isoformat().replace(":", "-")
-                if path.suffix == ".json":
-                    path.parent.mkdir(parents=True, exist_ok=True)
-                    file_results_aggregated = path.with_name(
-                        f"{path.stem}_{self.date_id}.json"
-                    )
-                else:
-                    path = path.joinpath(
-                        self.general_config_tracker.model_name_sanitized
-                    )
-                    path.mkdir(parents=True, exist_ok=True)
-                    file_results_aggregated = path.joinpath(
-                        f"results_{self.date_id}.json"
-                    )
-
+                file_results_aggregated = path.joinpath(f"results_{self.date_id}.json")
                 file_results_aggregated.open("w", encoding="utf-8").write(dumped)
 
                 if self.api and self.push_results_to_hub:
@@ -298,12 +285,6 @@ class EvaluationTracker:
                 eval_logger.info(f"Saving per-sample results for: {task_name}")
 
                 path = Path(self.output_path if self.output_path else Path.cwd())
-                if path.suffix == ".json":
-                    path = path.parent
-                else:
-                    path = path.joinpath(
-                        self.general_config_tracker.model_name_sanitized
-                    )
                 path.mkdir(parents=True, exist_ok=True)
 
                 file_results_samples = path.joinpath(
